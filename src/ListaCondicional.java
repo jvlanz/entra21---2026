f3e5612cb75ea6999bdf824c367e847643c8eb85
João Lanznaster <joao63466776@alunos.sc.senac.br>
2026-04-02 14:58:27 -0300

    Teste 1

--- src/ListaCondicional.java
+++ src/ListaCondicional.java
@@ -1,9 +1,201 @@
+import java.util.Scanner;
 
 public class ListaCondicional {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	static void ex1(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Digite um numero : ");
+		int valor=sc.nextInt();
+		if (valor%2==0) {
+			System.out.println("Este valor é par");
+		}
+		else {
+			System.out.println("Este valor é impar");
+		}
+	}
+		static void ex2(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite um numero : ");
+			int n1=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n2=sc.nextInt();
+			if (n1>n2) {
+				System.out.println("O maior numero é : "+n1);
+			}
+			else {
+				System.out.println("O maior numero é : "+n2);
+			}
+		}
+		static void ex3(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite um numero : ");
+			int valor=sc.nextInt();
+			if (valor>0) {
+				System.out.println("Este valor é positivo");
+			}
+			else if (valor<0){
+				System.out.println("Este valor é negativo");
+			}
+			else {
+				System.out.println("Este valor é zero");
+			}
+		}
+		static void ex4(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite a sua nota : ");
+			int nota=sc.nextInt();
+			if (nota>=6) {
+				System.out.println("Aprovado");
+			}
+			else {
+				System.out.println("Reprovado");
+			}
+		}
+		static void ex5(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite a sua idade: ");
+			int idade=sc.nextInt();
+			if (idade>=16) {
+				System.out.println("Você pode votar");
+			}
+			else {
+				System.out.println("Você não pode votar");
+			}
+		}
+		static void ex6(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite um numero : ");
+			int n1=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n2=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n3=sc.nextInt();
+			if (n1>n2&&n1>n3) {
+				System.out.println("O maior numero é : "+n1);
+			}
+			else if (n2>n1&&n2>n3){
+				System.out.println("O maior numero é : "+n2);
+			}
+			else {
+				System.out.println("O maior numero é : "+n3);
+			}
+		}
+		static void ex7(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite a sua nota : ");
+			int nota=sc.nextInt();
+			if (nota>=9) {
+				System.out.println("Excelente");
+			}
+			else if (nota>=7){
+				System.out.println("Bom");
+			}
+			else if (nota>=5) {
+				System.out.println("Regular");
+			}
+			else {
+				System.out.println("Insuficiente");
+			}
+		}
+		static void ex8(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite um numero : ");
+			int n1=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n2=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n3=sc.nextInt();
+		
+			if (n1+n2>n3&&n2+n3>n1&&n2+n3>n1) {
+				System.out.println("Pode ser um triangulo");
+			}
+			else {
+				System.out.println("O triangulo nao existe");
+			}
+		}
+		static void ex9(){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Digite um numero : ");
+			int n1=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n2=sc.nextInt();
+			System.out.println("Digite outro numero : ");
+			int n3=sc.nextInt();
+			
+			if (n1==n2&&n1==n3) {
+				System.out.println("O triangulo é equilatero");
+			}
+			else if (n1!=n2&&n1!=n3&&n2!=n3) {
+				System.out.println("O triangulo é escaleno");
+			}
+			else {
+				System.out.println("O triangulo é isosceles");
+			}
+		}
+			static void ex10() {
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Digite o usuario : ");
+				String usuario=sc.nextLine();
+				System.out.println("Digite a senha : ");
+				String senha=sc.nextLine();
+				if (usuario=="joao"&&senha=="123") {
+					System.out.println("Acesso Permitido");
+				}
+				else {
+					System.out.println("Acesso Negado");
+				}
+			
+		}
+			static void ex11() {
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Digite um ano : ");
+				int ano=sc.nextInt();
+				if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+					System.out.println("O ano é bissexto");
+				}
+			}
+			static void ex12(){
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Digite um numero : ");
+				float n1=sc.nextInt();
+				System.out.println("Digite outro numero : ");
+				float n2=sc.nextInt();
+				System.out.println("Digite uma operacao : ");
+				char operacao=sc.nextLine().charAt(0);
+				if (operacao=='+') {
+					System.out.println("A soma dos numeros é : "+(n1+n2));
+				}
+				else if (operacao=='-') {
+					System.out.println("A subtracao dos numeros é : "+(n1-n2));
+				}
+				else if (operacao=='*') {
+					System.out.println("A multiplicacao dos numeros é : "+(n1*n2));
+				}
+				else if (operacao=='/') {
+					System.out.println("A divisao dos numeros é : "+(n1/n2));
+				}
+			}
+			static void ex13() {
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Digite o seu salario : ");
+				float salario=sc.nextInt();
+				if (salario<2000) {
+					System.out.println("O salario será : "+salario*0.9f);
+				}
+				else if (salario>=2000&&salario<5000) {
+					System.out.println("O salario será : "+salario*0.85f);
+				}
+				else if (salario>=5000&&salario<10000) {
+					System.out.println("O salario será : "+salario*0.7f);
+				}
+				else if (salario>=10000) {
+					System.out.println("O salario será : "+salario*0.65f);
+				}
+			}
 	}
 
-}
+
